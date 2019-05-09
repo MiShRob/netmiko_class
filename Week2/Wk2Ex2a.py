7ee45025811b61169acc21b71ef3f57eff00d2fa
Mike Robinson <mike.robinson0001@gmail.com>
2019-05-08 17:52:08 -0700

    Added time checking to script

--- Week2/Wk2Ex2a.py
+++ Week2/Wk2Ex2a.py
@@ -1,5 +1,6 @@
 from getpass import getpass
 from netmiko import ConnectHandler
+import time
 
 
 device1 = {
@@ -12,8 +13,16 @@ device1 = {
 
 
 net_connect = ConnectHandler(**device1)
+start_clk = time.time()
 output = net_connect.send_command('show lldp neighbors detail')
+cmd1_clk = time.time()
 print(output)
 
+start_clk2 = time.time()
 output = net_connect.send_command('show lldp neighbors detail', delay_factor=8)
+cmd2_clk = time.time()
 print(output)
+
+print("=-" * 40)
+print(f"Execution of first send_command took {cmd1_clk - start_clk} seconds")
+print(f"Execution of second send_command took {cmd2_clk - start_clk2} seconds")
